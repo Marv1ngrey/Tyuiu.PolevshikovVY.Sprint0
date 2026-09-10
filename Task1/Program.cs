25332e351ca928d60ef0747c58a6751ee40a3b2c
v <v8po1ev@yandex.ru>
2026-09-10 12:35:57 +0500

    task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string name = "Василий";
+int age = 18;
+Console.WriteLine("Здраствуйте, меня зовут" + name + "мне" + age + "лет");
+Console.ReadKey();
