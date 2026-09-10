6ce928629cc513eb38e6fdb00574defde7166d72
v <v8po1ev@yandex.ru>
2026-09-10 12:47:51 +0500

    Task1

--- Task0/obj/Debug/net8.0/Tyuiu.PolevshikovVY.Sprint0.Task0.v0.AssemblyInfo.cs
+++ Task0/obj/Debug/net8.0/Tyuiu.PolevshikovVY.Sprint0.Task0.v0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.PolevshikovVY.Sprint0.Task0.v0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f402cb4e1066c828930e7ec84811d56b57d775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe3ca201a5bc9d3ec2706274c37348fb2e40065f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.PolevshikovVY.Sprint0.Task0.v0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.PolevshikovVY.Sprint0.Task0.v0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string name = "Василий";
-int age = 18;
-Console.WriteLine("Здраствуйте, меня зовут" + name + "мне" + age + "лет");
-Console.ReadKey();
+﻿namespace Tyuiu.PolevshikovVY.Sprint0.Task1.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Василий";
+        int age = 18;
+        Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет");
+        Console.ReadKey();
+    }
+}
