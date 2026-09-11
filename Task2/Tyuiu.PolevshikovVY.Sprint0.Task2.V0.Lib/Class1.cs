1ed929e0195665fd27d3404d5cfff4bf9ca4e342
v <v8po1ev@yandex.ru>
2026-09-11 20:15:31 +0500

    refactor(project): restructure project layout and add solution file
    Remove obsolete Task0 and Task1 directories along with build artifacts
    (bin/obj). Introduce nested project directories for Task0 and Task1,
    add a new Task2 directory, and create a central solution file to
    improve project organization and build management.

---
+++ Task2/Tyuiu.PolevshikovVY.Sprint0.Task2.V0.Lib/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.PolevshikovVY.Sprint0.Task1.V0.Lib;
+
+public class Class1
+{
+
+}
---
+++ Task2/Tyuiu.PolevshikovVY.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.PolevshikovVY.Sprint0.Task1.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
