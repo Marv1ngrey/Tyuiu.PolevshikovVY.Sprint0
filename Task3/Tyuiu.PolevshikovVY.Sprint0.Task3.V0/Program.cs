bcea06169801ad896dbae0a5c8afd1d77c527995
v <v8po1ev@yandex.ru>
2026-09-12 11:09:04 +0500

    Task3

--- Task3/Tyuiu.PolevshikovVY.Sprint0.Task3.V0/Program.cs
+++ Task3/Tyuiu.PolevshikovVY.Sprint0.Task3.V0/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Tyuiu.PolevshikovVY.Sprint0.Task3.V0;
+﻿using Tyuiu.PolevshikovVY.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.PolevshikovVY.Sprint0.Task3.V0;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
     }
 }
